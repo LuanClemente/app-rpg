7b7fad6c93fca910836e0e0a34b9b50a717a20fb
Luan Clemente Costa <79475114+LuanClemente@users.noreply.github.com>
2025-10-03 17:15:22 +0000

    Atualizando, agora vai

--- src/main/java/com/apprpg/config/SecurityConfig.java
+++ src/main/java/com/apprpg/config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
-            "https://app-rpg-production.up.railway.app" // substitui pelo domínio real do frontend
+            "https://app-rpg-production.up.railway.app"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
